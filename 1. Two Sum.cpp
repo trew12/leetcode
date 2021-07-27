2d90adfdbf326bc1b0c72ad2570a8b63b8be6474
Anton Volodin <64497667+trew12@users.noreply.github.com>
2021-07-27 19:34:41 +0300

    Create 1. Two Sum.cpp

---
+++ 1. Two Sum.cpp
@@ -0,0 +1,19 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> val_to_ind;
+        for (int i = 0; i < nums.size(); i++){
+            int diff = target - nums[i];
+            if (val_to_ind.count(diff) > 0){
+                return vector<int> {i, val_to_ind[diff]};
+            }
+            val_to_ind.insert({nums[i], i});
+        }
+        return vector<int> {};
+    }
+};
