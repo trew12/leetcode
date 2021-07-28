6410ebbb97c25fe40c0d328574c307f158038ead
Anton Volodin <64497667+trew12@users.noreply.github.com>
2021-07-28 20:49:37 +0300

    Create 1929. Concatenation of Array.cpp

---
+++ 1929. Concatenation of Array.cpp
@@ -0,0 +1,12 @@
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+Specifically, ans is the concatenation of two nums arrays.
+Return the array ans.
+*/
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        return nums;
+    }
+};
